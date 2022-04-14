8dcccd08ef7498af1423ce36afdb172a6d79f810
AshokBabuMannam <98141316+AshokBabuMannam@users.noreply.github.com>
2022-04-13 19:56:19 -0500

    Update icecream.js

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -44,3 +44,24 @@ exports.icecream_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+   // Handle Costume create on POST.
+exports.icecream_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new icecream();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.flavor = req.body.flavor;
+    document.price = req.body.price;
+    document.quantity = req.body.quantity;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
