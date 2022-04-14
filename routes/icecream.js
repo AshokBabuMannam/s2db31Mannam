39f7b3edabc5cdb0548b9d7977cf03c101271c2f
AshokBabuMannam <98141316+AshokBabuMannam@users.noreply.github.com>
2022-04-13 19:46:36 -0500

    Update icecream.js

--- routes/icecream.js
+++ routes/icecream.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const icecream_controlers= require('../controllers/icecream');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('icecream', { title: 'search result'});
-});
+router.get('/', costume_controlers.costume_view_all_Page );
 
 module.exports = router;
