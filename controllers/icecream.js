c16adb474aebdebc2e91db97bbd45329c95dc162
AshokBabuMannam <98141316+AshokBabuMannam@users.noreply.github.com>
2022-04-13 19:46:32 -0500

    Update icecream.js

--- controllers/icecream.js
+++ controllers/icecream.js
@@ -31,3 +31,16 @@ exports.icecream_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
    };
+
+   // VIEWS
+// Handle a show all view
+exports.icecream_view_all_Page = async function(req, res) {
+    try{
+    theCostumes = await Costume.find();
+    res.render('icecream', { title: 'icecream Search Results', results: theCostumes });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
