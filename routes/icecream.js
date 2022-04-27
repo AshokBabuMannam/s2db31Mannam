203f56e798c0a25b4158fc5a3947729948e49c57
AshokBabuMannam <98141316+AshokBabuMannam@users.noreply.github.com>
2022-04-27 16:07:35 -0500

    Added files

--- routes/icecream.js
+++ routes/icecream.js
@@ -15,10 +15,10 @@ router.get('/', icecream_controlers.icecream_view_all_Page );
 /// COSTUME ROUTES ///
 router.get('/detail', icecream_controlers.icecream_view_one_Page); 
 
-router.get('/create', icecream_controlers.icecream_create_Page); 
+router.get('/create',secured, icecream_controlers.icecream_create_Page); 
 
 router.get('/update',secured, icecream_controlers.icecream_update_Page); 
 
-router.get('/delete', icecream_controlers.icecream_delete_Page);
+router.get('/delete',secured, icecream_controlers.icecream_delete_Page);
 
 module.exports = router;
